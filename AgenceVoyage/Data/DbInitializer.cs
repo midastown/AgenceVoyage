22e65965168c617fa37fd80cb31e0bbc68fde6c6
midastown <mehdihachimi1@gmail.com>
2022-01-08 18:12:46 -0500

    modified layout and added a db init

---
+++ AgenceVoyage/Data/DbInitializer.cs
@@ -0,0 +1,61 @@
+﻿using AgenceVoyage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgenceVoyage.Data
+{
+    public class DbInitializer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        internal static void Initialize(VoyageContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Clients.Any())
+            {
+                return;
+            }
+
+            var clients = new Client[]
+            {
+                new Client{nom="Mehdi", prenom="Hachimi", adresse="11 rue champ", ville="montreal", codePostal="h2v3u3", modePaiement="carte"}
+            };
+
+            foreach (Client c in clients)
+            {
+                context.Clients.Add(c);
+            }
+
+            context.SaveChanges();
+
+            var forfaits = new Forfait[]
+            {
+                new Forfait{destination="Maroc", prix=12.2, duree=3}
+            };
+
+            foreach (Forfait f in forfaits)
+            {
+                context.Forfaits.Add(f);
+            }
+
+            context.SaveChanges();
+
+            //var voyages = new Voyage[]
+            //{
+            //    new Voyage{}
+            //};
+
+            //foreach (Voyage v in voyages)
+            //{
+            //    context.Voyages.Add(v);
+            //}
+
+            //context.SaveChanges();
+        }
+    }
+}
--- AgenceVoyage/Program.cs
+++ AgenceVoyage/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AgenceVoyage.Data;
 
 namespace AgenceVoyage
 {
@@ -13,7 +15,28 @@ namespace AgenceVoyage
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDatabaseIfNotFound(host);
+            host.Run();
+        }
+
+        private static void CreateDatabaseIfNotFound(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                
+                try
+                {
+                    var context = services.GetRequiredService<VoyageContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error has occured creating the DB");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
