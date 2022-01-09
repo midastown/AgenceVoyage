95d647de6a59bde3aea527db7950f79019ced53f
supermoi1974 <9335221@bdeb.qc.ca>
2022-01-09 18:16:42 -0500

    Modification HomeController (Index()) pour avoir accès aux données

--- AgenceVoyage/Controllers/HomeController.cs
+++ AgenceVoyage/Controllers/HomeController.cs
@@ -1,26 +1,30 @@
-﻿using AgenceVoyage.Models;
+﻿using AgenceVoyage.Data;
+using AgenceVoyage.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace AgenceVoyage.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly VoyageContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(VoyageContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Forfaits.ToListAsync());
         }
 
         public IActionResult Privacy()
